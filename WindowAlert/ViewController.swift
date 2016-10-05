fa14cb37eee7f7b99b4d6f068706c415d93872fa
Bondar Yaroslav <ybondar@smedialink.com>
2016-10-05 09:44:09 +0300

    Initial Commit

---
+++ WindowAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WindowAlert
+//
+//  Created by Yaroslav Bondar on 05.10.16.
+//  Copyright © 2016 SMediaLink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
